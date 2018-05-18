0a624345467e7ec09366a29e94a22efbc9ecaf67
vishal <vishal.kumar1145@gmail.com>
2018-05-18 18:31:44 +0530

    google search fix

--- console/cron/crawler_tasks.php
+++ console/cron/crawler_tasks.php
@@ -15,6 +15,7 @@ require_once '../../includes/class/phpwebdriver/WebDriver.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
+
 require_once 'PHPMailer/src/PHPMailer.php';
 require_once 'PHPMailer/src/SMTP.php';
 require_once 'PHPMailer/src/Exception.php';
@@ -25,14 +26,10 @@ use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-$dbo		= get_dbo();
-//$settings	= get_settings();
-$html		= new simple_html_dom();
+$dbo        = get_dbo();
+$html       = new simple_html_dom();
 $_mail_auto = false;
 
-// if (!$settings['system_working']) {
-// 	exit;
-// }
 
 write_log_to_file("Closing all Chrome Drivers if working backround...\n");
 `taskkill /im chromedriver.exe /f`;
@@ -44,145 +41,131 @@ sleep(1);
 
 //Function to click on the Link
 
- function checkLink($one_block,$site_domain,$webdriver){
-
-	$h3_element =    $one_block->findElementBy(LocatorStrategy::cssSelector, 'h3');
-	$a_eleamnt =  $h3_element->findElementBy(LocatorStrategy::cssSelector, 'a');
-
-	$is_domain_exists = strpos($a_eleamnt->getAttribute('href'), $site_domain);
-
-	$link_clicked = false;
-	if($is_domain_exists==true)
-			{
-							$ithours_link = $a_eleamnt->getAttribute('href');
-							$a_eleamnt->click();
-							$link_clicked = true;
-							sleep(15);
-						    $webdriver->closeWindow();
-							$webdriver->close();
-							$link_clicked = true;
-							
-				
-			}
-
-			return $link_clicked;
-
- }
-
-
-while(true)
+function clickLink($site_domain, $webdriver)
 {
-	write_log_to_file('Before Executing query .\n');	
+	$link_clicked = false;
+	$res_array = $webdriver->findElementsBy(LocatorStrategy::cssSelector, '.rc');
+        foreach ($res_array as $one_block) {
 
-		$Query = 'SELECT * FROM `crawler_task` WHERE  status ="NEW" ORDER BY rand() LIMIT 1';
-		$Query_Results	= $dbo->execute($Query);
-
-		if (!sizeof($Query_Results))
+    $h3_element = $one_block->findElementBy(LocatorStrategy::cssSelector, 'h3');
+    $a_eleamnt  = $h3_element->findElementBy(LocatorStrategy::cssSelector, 'a');
+    $is_domain_exists = strpos($a_eleamnt->getAttribute('href'), $site_domain);
+    if ($is_domain_exists == true) {
+        $a_eleamnt->click();
+        sleep(15);
+        $webdriver->closeWindow();
+        $webdriver->close();
+		$link_clicked = true;
 		break;
-
-		$task = $Query_Results[0];
-
-		echo 'task id '.$task['id'];
-		if ($task['type'] == 'search') {
-
-	    $webdriver	= new WebDriver($GLOBALS['config']['webdriver']['host'], $GLOBALS['config']['webdriver']['port']);
-		$webdriver->connect('chrome', '', array
-		(
-			'proxy'=>array
-			(
-				'proxyType'		=> 'manual',
-				'httpProxy'		=> $bot['domain'].':'.$bot['http_port'],
-				'sslProxy'		=> $bot['domain'].':'.$bot['http_port']
-			),
-			'chromeOptions'=>array('args'=>array('user-data-dir='.$GLOBALS['config']['bots_user_data_dir'].$bot['id'].'/','disable-infobars','ignore-certificate-errors','disable-notifications','load-extension='.$GLOBALS['config']['chrome_multipass_extension_dir']))
-		));
-
-		$webdriver->windowMaximize();
-		$webdriver->setImplicitWaitTimeout(10000);
-		$webdriver->setSpeed('SLOW');
-		
-		$webdriver->get($task['query']);
-
-		sleep(15);
-
-		$site_domain = $task['target_domain'];
-
-		$link_clicked= false;
-
-		$res_array = $webdriver->findElementsBy(LocatorStrategy::cssSelector, '.rc');
-		
-		foreach ($res_array as $one_block) {
-			
-			checkLink($one_block,$site_domain,$webdriver);
-			
-		}
-		if($link_clicked == false)
-			{
-			//$next_pages = $webdriver->findElementBy(LocatorStrategy::cssSelector, '.f1');
-
-			$table_data	= $webdriver->findElementBy(LocatorStrategy::id, 'nav');
-			$tbody_data	= $table_data->findElementBy(LocatorStrategy::cssSelector, 'tbody');
-			$tr_data	= $tbody_data->findElementBy(LocatorStrategy::cssSelector, 'tr');
-			$td_data	= $tr_data->findElementsBy(LocatorStrategy::cssSelector, 'td');
-			// $td_1st_data_deleted = array_shift($td_data);
-			// $td_2nd_data_deleted = array_shift($td_data);
-
-				 $a_eleamnt =    $td_data[2]->findElementBy(LocatorStrategy::cssSelector, 'a');
-				 $ithours_link = $a_eleamnt->getAttribute('href');
-				 $a_eleamnt->click();
-				 sleep(15);
-
-				 $res_array = $webdriver->findElementsBy(LocatorStrategy::cssSelector, '.rc');
-		
-		    foreach ($res_array as $one_block){
-
-			if($link_clicked == true)
-				break;
-
-			checkLink($one_block,$site_domain,$webdriver);
-			
-		    }
-			
-			}
-
-			if($link_clicked == false)
-			{
-			//$next_pages = $webdriver->findElementBy(LocatorStrategy::cssSelector, '.f1');
-
-			$table_data	= $webdriver->findElementBy(LocatorStrategy::id, 'nav');
-			$tbody_data	= $table_data->findElementBy(LocatorStrategy::cssSelector, 'tbody');
-			$tr_data	= $tbody_data->findElementBy(LocatorStrategy::cssSelector, 'tr');
-			$td_data	= $tr_data->findElementsBy(LocatorStrategy::cssSelector, 'td');
-			// $td_1st_data_deleted = array_shift($td_data);
-			// $td_2nd_data_deleted = array_shift($td_data);
-
-				 $a_eleamnt =    $td_data[3]->findElementBy(LocatorStrategy::cssSelector, 'a');
-				 $ithours_link = $a_eleamnt->getAttribute('href');
-				 $a_eleamnt->click();
-
-				 sleep(15);
-
-				 $res_array = $webdriver->findElementsBy(LocatorStrategy::cssSelector, '.rc');
-		
-		    foreach ($res_array as $one_block){
-
-			if($link_clicked == true)
-				break;
-
-				$link_clicked = checkLink($one_block,$site_domain,$webdriver);
-
-				echo $link_clicked;
-			
-		    }
-			
-			}
-
-			
-		
-
 	}
-
 	
+}
+
+return $link_clicked;
+
+}
+
+function openNextPage($pageNo, $webdriver)
+{
+    $table_data = $webdriver->findElementBy(LocatorStrategy::id, 'nav');
+    $tbody_data = $table_data->findElementBy(LocatorStrategy::cssSelector, 'tbody');
+    $tr_data    = $tbody_data->findElementBy(LocatorStrategy::cssSelector, 'tr');
+    $td_data    = $tr_data->findElementsBy(LocatorStrategy::cssSelector, 'td');
+    $a_eleamnt  = $td_data[$pageNo]->findElementBy(LocatorStrategy::cssSelector, 'a');
+    $a_eleamnt->click();
+    sleep(15);
+}
+
+
+while (true) {
+    write_log_to_file('Before Executing query .\n');
+    
+    $Query         = 'SELECT * FROM `crawler_task` ORDER BY rand() LIMIT 1';
+    $Query_Results = $dbo->execute($Query);
+    if (!sizeof($Query_Results))
+        break;
+    
+    $task = $Query_Results[0];
+	echo 'task id ' . $task['id'];
+	
+    if ($task['type'] == 'search') {
+
+	if($task['engine'] == 'google'){
+        $webdriver = new WebDriver($GLOBALS['config']['webdriver']['host'], $GLOBALS['config']['webdriver']['port']);
+        $webdriver->connect('chrome', '', array(
+            'proxy' => array(
+                'proxyType' => 'manual',
+                'httpProxy' => $bot['domain'] . ':' . $bot['http_port'],
+                'sslProxy' => $bot['domain'] . ':' . $bot['http_port']
+            ),
+            'chromeOptions' => array(
+                'args' => array(
+                    'user-data-dir=' . $GLOBALS['config']['bots_user_data_dir'] . $bot['id'] . '/',
+                    'disable-infobars',
+                    'ignore-certificate-errors',
+                    'disable-notifications',
+                    'load-extension=' . $GLOBALS['config']['chrome_multipass_extension_dir']
+                )
+            )
+        ));
+        
+        $webdriver->windowMaximize();
+        $webdriver->setImplicitWaitTimeout(10000);
+        $webdriver->setSpeed('SLOW');
+		
+		$query =  "https://www.google.com/search?q=".$task['query'];
+
+        $webdriver->get($query);
+        
+        sleep(15);
+        
+        $site_domain = $task['target_domain'];
+        
+        $link_clicked = clickLink($site_domain,$webdriver);
+		
+        if ($link_clicked == false) {
+            openNextPage(2, $webdriver);
+			$link_clicked = clickLink($site_domain,$webdriver);
+        }
+        
+        if ($link_clicked == false) {
+            openNextPage(3, $webdriver);
+            $link_clicked = clickLink($site_domain,$webdriver);
+            
+		}
+	}if($task['engine'] == 'yahoo'){
+
+		$webdriver = new WebDriver($GLOBALS['config']['webdriver']['host'], $GLOBALS['config']['webdriver']['port']);
+        $webdriver->connect('chrome', '', array(
+            'proxy' => array(
+                'proxyType' => 'manual',
+                'httpProxy' => $bot['domain'] . ':' . $bot['http_port'],
+                'sslProxy' => $bot['domain'] . ':' . $bot['http_port']
+            ),
+            'chromeOptions' => array(
+                'args' => array(
+                    'user-data-dir=' . $GLOBALS['config']['bots_user_data_dir'] . $bot['id'] . '/',
+                    'disable-infobars',
+                    'ignore-certificate-errors',
+                    'disable-notifications',
+                    'load-extension=' . $GLOBALS['config']['chrome_multipass_extension_dir']
+                )
+            )
+        ));
+        
+        $webdriver->windowMaximize();
+        $webdriver->setImplicitWaitTimeout(10000);
+        $webdriver->setSpeed('SLOW');
+		
+		$query =  "https://in.search.yahoo.com/search?p=".$task['query'];
+
+		$webdriver->get($query);
+		
 	}
+
+    } else if ($task['type'] == 'directhit') {
+
+	 }
+}
 
 ?>
